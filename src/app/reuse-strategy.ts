d3da11f9672228c4c608877bd1cb7671307fe0ac
Alex Parloti <parloti@gmail.com>
2026-01-05 16:06:43 -0300

    fix duplicated on session

--- src/app/reuse-strategy.ts
+++ src/app/reuse-strategy.ts
@@ -1,25 +1,9 @@
 import {
   ActivatedRouteSnapshot,
   BaseRouteReuseStrategy,
-  DetachedRouteHandle,
 } from '@angular/router';
 
 export class CustomReuseStrategy extends BaseRouteReuseStrategy {
-  override shouldDetach(route: ActivatedRouteSnapshot): boolean {
-    return super.shouldDetach(route);
-  }
-  override store(
-    route: ActivatedRouteSnapshot,
-    handle: DetachedRouteHandle
-  ): void {
-    return super.store(route, handle);
-  }
-  override shouldAttach(route: ActivatedRouteSnapshot): boolean {
-    return super.shouldAttach(route);
-  }
-  override retrieve(route: ActivatedRouteSnapshot): DetachedRouteHandle | null {
-    return super.retrieve(route);
-  }
   override shouldReuseRoute(
     future: ActivatedRouteSnapshot,
     curr: ActivatedRouteSnapshot
